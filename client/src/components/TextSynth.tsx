3fa07bd668f533a23d9865ed5d32d86eff583ee1
Evan Conway <43253820+evanconway@users.noreply.github.com>
2024-05-09 00:40:57 -0400

    move on type sound to hooks

--- client/src/components/TextSynth.tsx
+++ client/src/components/TextSynth.tsx
@@ -1,52 +1,19 @@
-import { useEffect, useState } from "react";
-import { Synth } from "tone";
-
-const pitches = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
+import { useState } from "react";
+import { useSynthKeyDown } from "../hooks";
 
 const TextSynth = () => {
   const [text, setText] = useState("");
-  const [synth, setSynth] = useState<Synth | null>(null);
 
-  useEffect(() => {
-    const newSynth = new Synth({
-      oscillator: {
-        type: "fmsquare",
-        modulationType: "sawtooth",
-        modulationIndex: 3,
-        harmonicity: 3.4,
-      },
-      envelope: {
-        attack: 0.001,
-        decay: 0.1,
-        sustain: 0.1,
-        release: 0.1,
-      },
-    }).toDestination();
-    setSynth(newSynth);
-  }, [setSynth]);
-
-  useEffect(() => {
-    if (synth === null) return;
-    const onKeyDown = (ev: KeyboardEvent) => {
-      if (
-        ev.key.length !== 1 &&
-        !["backspace", "enter"].includes(ev.key.toLowerCase())
-      )
-        return;
-      synth.triggerAttackRelease(
-        pitches[Math.floor(Math.random() * pitches.length)],
-        "8n",
-      );
-    };
-    window.addEventListener("keydown", onKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", onKeyDown);
-    };
-  }, [synth]);
+  useSynthKeyDown();
 
   return (
     <div>
+      <textarea
+        style={{ width: "300px", height: "300px" }}
+        placeholder="Enter your text here..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
       <input
         type="text"
         placeholder="Enter your text here..."
---
+++ client/src/hooks.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Synth } from "tone";
+
+const pitches = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
+
+export const useSynth = () => {
+  const [synth] = useState<Synth>(
+    new Synth({
+      oscillator: {
+        type: "fmsquare",
+        modulationType: "sawtooth",
+        modulationIndex: 3,
+        harmonicity: 3.4,
+      },
+      envelope: {
+        attack: 0.001,
+        decay: 0.1,
+        sustain: 0.1,
+        release: 0.1,
+      },
+    }).toDestination(),
+  );
+
+  return synth;
+};
+
+export const useSynthKeyDown = () => {
+  const synth = useSynth();
+
+  useEffect(() => {
+    const onKeyDown = (ev: KeyboardEvent) => {
+      if (
+        ev.key.length !== 1 &&
+        !["backspace", "enter"].includes(ev.key.toLowerCase())
+      )
+        return;
+      synth.triggerAttackRelease(
+        pitches[Math.floor(Math.random() * pitches.length)],
+        "8n",
+      );
+    };
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, []);
+};
